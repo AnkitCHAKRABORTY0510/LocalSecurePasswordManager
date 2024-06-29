739042520b03f063f6fa96efbda806fc54e23442
AnkitCHAKRABORTY0510 <ankitchakraborty0510@gmail.com>
2024-06-30 01:55:02 +0530

    git commit login and create 1

--- src/main/java/com/mycompany/securepasswordmanager/NewUserController.java
+++ src/main/java/com/mycompany/securepasswordmanager/NewUserController.java
@@ -5,14 +5,45 @@
 package com.mycompany.securepasswordmanager;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author mac
  */
-public class NewUserController {
-     @FXML
+    
+    
+    public class NewUserController {
+    
+    @FXML
+    private TextField UserNameTextField;
+    
+    @FXML
+    private PasswordField PasswordTextField;
+    
+    @FXML
+    private Label CreateUserMessage;
+    
+    public void LoginButtonOn(ActionEvent e)
+    {
+      
+        if (UserNameTextField.getText().isBlank()==false && PasswordTextField.getText().isBlank()==false)
+        {
+            CreateUserMessage.setText("You try to create user");
+        }
+        else
+        {
+            CreateUserMessage.setText("Please Enter username and password !!");
+        }
+    }
+    
+    //function to change between login screen and create 
+    //user screen in using goTologin button 
+    @FXML
     private void switchToLogin() throws IOException {
         App.setRoot("login");
     }
